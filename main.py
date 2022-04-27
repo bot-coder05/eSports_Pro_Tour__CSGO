6bac69d6bce92f29eae5f9a9d39151606ad2e856
kent <john.kenneth2525@yahoo.com>
2022-04-27 22:39:04 +0400

    enhanced .help visuals

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import os
+from colored import fg, attr
 from schedule import Schedule
+
+
+def color_word(color, word):
+    return fg(color) + word + attr('reset')
 
 
 if __name__ == '__main__':
@@ -27,9 +32,10 @@ if __name__ == '__main__':
                 display = cal.make_calendar()
 
         elif command == ".help":
-            display = """
-.calendar: changes display to the calendar
-    .week: proceeds to the next week
+            display = f"""
+{color_word('light_green', '.calendar')}: changes display to the calendar
+    {color_word('light_green', '.week')}: proceeds to the next week
+    {color_word('light_green', '.month')} {color_word('red', '[month name]')}: view [month name]
 
             """
 
